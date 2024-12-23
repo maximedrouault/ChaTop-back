f8d1487cc1d0a2048542882cead0667bcc6a96ef
maximedrouault <maximedrouault@gmail.com>
2024-12-23 17:01:00 +0100

    first commit

---
+++ src/main/java/org/chatop/chatopback/ChaTopBackApplication.java
@@ -0,0 +1,13 @@
+package org.chatop.chatopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaTopBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaTopBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chatop/chatopback/ChaTopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chatop.chatopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaTopBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
