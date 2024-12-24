a55aad1e9574c5e72c73cff50eddb70cd95dc6ec
maximedrouault <maximedrouault@gmail.com>
2024-12-24 16:50:51 +0100

    Path GET /user/:id with Swagger Doc added

--- src/main/java/org/chatop/chatopback/controller/UserController.java
+++ src/main/java/org/chatop/chatopback/controller/UserController.java
@@ -1,5 +1,10 @@
 package org.chatop.chatopback.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.chatop.chatopback.dto.UserDto;
 import org.chatop.chatopback.repository.UserRepository;
@@ -18,6 +23,21 @@ public class UserController {
 
 
     @GetMapping("/user/{id}")
+    @Operation(summary = "Get a user by id", parameters = {
+            @Parameter(name = "id", description = "ID of the user to be retrieved", required = true, example = "1"),
+    }, responses = {
+            @ApiResponse(responseCode = "200", description = "User found", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserDto.class))),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "application/json",
+                    schema = @Schema(example = """
+                    {
+                        "timestamp": "2024-12-24T15:37:37.870+00:00",
+                        "status": 404,
+                        "error": "Not Found",
+                        "path": "/user/0"
+                    }
+                    """)))
+    })
     public ResponseEntity<UserDto> getUserById(@PathVariable Integer id) {
         return userRepository.findById(id)
                 .map(user -> ResponseEntity.ok(new UserDto(
