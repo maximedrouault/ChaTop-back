b3b42f87f66baa69d62ddc727b5f824f624f6d06
maximedrouault <maximedrouault@gmail.com>
2024-12-24 10:37:35 +0100

    Spring boot project created

--- src/main/java/org/chatop/chatopback/ChaTopBackApplication.java
+++ src/main/java/org/chatop/chatopback/ChaTopBackApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChaTopBackApplication {
+public class ChaTopBackApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(ChaTopBackApplication.class, args);
